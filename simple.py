2c6b4ed173b7c2724a33e3c80d70c8f8196acf1a
Abdul Malik <128873080+ABDULMALIK222-cloud@users.noreply.github.com>
2024-12-16 02:19:54 +0530

    Create simple.py
    median

---
+++ simple.py
@@ -0,0 +1,16 @@
+import math
+import re
+import os
+import random
+import sys
+
+def findMedian(arr):
+  arr.sort()
+  return arr[n//2]
+if__name__=="__main__":
+  fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+  n = int(input().strip())
+
+  a = list(map(int, input().rstrip().split()))
+  result = findMedian(arr)
