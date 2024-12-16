bc945b3b718206a40ab0b1e33fcdd4e6e57c4816
Abdul Malik <128873080+ABDULMALIK222-cloud@users.noreply.github.com>
2024-12-17 00:23:25 +0530

    Create p.py

---
+++ p.py
@@ -0,0 +1,7 @@
+# about collections from python 
+#first we have counter property from collection 
+from collections import counter
+
+li = [1,2,3,4,5];
+coun = collections.counter(li)
+coun = update.data(li)
