0c87ad6734ea5fb5d2b254e90ee91cfbc56ad445
Abdul Malik <128873080+ABDULMALIK222-cloud@users.noreply.github.com>
2024-12-16 16:16:09 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -44,5 +44,46 @@ if__name__=="__main__":
     fptr.write(str(result) + '\n')
 
     fptr.close()
+# program to swap the elements first element to last element
+def interchange(list):
+  size = len(list)
+  temp = list[0]
+  list[0] = list[size -1]
+  list[size -1] = temp
+  return list
 
+#driver code
+list = [1,2,3,4,5,6]
+print(interchange(list))
+
+# using tuple variable swappint hte first and last element in a list
+def interchange(list):
+  #storing first and last elements of list using get tuple variable
+  get = list[0],list[-1]
+  #swapping the variables
+  list[0],list[-1] = get
+  return list
+
+#driver code
+list = [1,2,3,4,5]
+print(interchange(list))
+
+#using  * operand swapping the elements
+def interchage(list):
+  start,*middle,end = list
+  list =[end,*middle,start]
+  return list
+
+#driver code
+list = [1,2,3,4,5]
+print(interchange(list))
+# program to slice the elements in the list
+def interchange(list):
+  #check if 2 elements are present in it or not
+  if(len(list))>2:
+    list = list[-1:] + list[1:-1]+list[:1]
+  return list
+#driver code
+list = [1,2,3,4,5]
+print(interchange(list))
     
